4a4467bbc8e12f03783f6aa221b0b9e4326c13ad
Wei-Ren Xue <weirenxue@gmail.com>
2021-11-01 12:39:31 +0800

    改為上傳 md 語法

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Octokit } = require("@octokit/core");
+const urljoin = require('url-join');
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
@@ -7,14 +8,17 @@ const main = async () => {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
     });
-
+    let blogUrl = 'https://weirenxue.github.io';
     let result = await admin.firestore().collection('articles').orderBy('count', 'desc').get();
     let data = [];
-    data.push('Blog URL:https://weirenxue.github.io');
+    data.push(`Blog：[薛惟仁筆記本](${blogUrl})`);
 
     result.forEach((doc) => {
-        if (doc.data().count <= 100) return
-        data.push(`${doc.data().count}|${doc.id}`);
+        if(doc.data().pathname === undefined)
+            data.push(`${doc.data().count}｜${doc.id}`);
+        else
+            data.push(`${doc.data().count}｜[${doc.id}](${urljoin(blogUrl, doc.data().pathname)})`);
+
     })
 
     const octokit = new Octokit({
@@ -23,8 +27,8 @@ const main = async () => {
 
     let response = await octokit.request('PATCH /gists/{gist_id}', {
         files: {
-            'ranking': {
-                content: data.join('\n')
+            'ranking.md': {
+                content: data.join('  \n')
             }
         },
         public: true
